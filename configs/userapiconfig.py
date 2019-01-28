07d42fcec62f49f8f1f5da9f8a2d0dd83d228e9d
Isaccliberty <8qllyhy@gmail.com>
2019-01-28 11:18:30 +0800

    first commit with build pass

---
+++ configs/userapiconfig.py
@@ -0,0 +1,16 @@
+﻿# API_INTERFACE = 'ehcomod'  # ehcomod <谜之屋专用> # webapi
+API_INTERFACE = 'webapi'  # ehcomod <谜之屋专用> # webapi
+UPDATE_TIME = 60
+
+# Webapi token
+TOKEN = "you own token"
+WEBAPI_URL = 'http://127.0.0.1:8000/api'
+NODE_ID = 1
+
+
+# Mysql
+MYSQL_CONFIG = 'configs/usermysql.json'
+
+# MUJSON API
+MUAPI_CONFIG = 'usermuapi.json'
+SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
